fba57636cb41c0641ab3a3f91aa65c55fad5b8ae
ori346 <ori4655@gmail.com>
2026-06-04 15:19:59 +0300

    increase num of retry for falky tests

--- hub/integration-tests/tests/mcp_servers/test_kafka_tools.py
+++ hub/integration-tests/tests/mcp_servers/test_kafka_tools.py
@@ -10,7 +10,7 @@ import pytest
 
 # Kafka tests are flaky in CI due to unstable Kind cluster environment
 # (resource-constrained broker, timing-sensitive connections).
-pytestmark = pytest.mark.flaky(reruns=3, reruns_delay=5)
+pytestmark = pytest.mark.flaky(reruns=10, reruns_delay=5)
 
 MCP_HEADERS = {
     "Accept": "application/json, text/event-stream",
